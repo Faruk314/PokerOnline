da9d562b6eed4e643ca09cb758c808f7ac4c7c73
Faruk Spahic <farukspahictz@gmail.com>
2024-12-15 12:15:42 +0100

    Added a middleware on backend

--- backend/src/routes/game.ts
+++ backend/src/routes/game.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { fetchRooms, getGameState } from "../controllers/game";
+import middleware from "../utils/middleware";
 
 const router = express.Router();
 
-router.get("/fetchRooms", fetchRooms);
+router.get("/fetchRooms", middleware, fetchRooms);
 
-router.post("/getGameState", getGameState);
+router.post("/getGameState", middleware, getGameState);
 
 export default router;
--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -1,5 +1,13 @@
 import { Server } from "socket.io";
 
+declare global {
+  namespace Express {
+    interface Request {
+      user?: VerifiedToken;
+    }
+  }
+}
+
 interface VerifiedToken {
   userId: number;
   userName: string;
