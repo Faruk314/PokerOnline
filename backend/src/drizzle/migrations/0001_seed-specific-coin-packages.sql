a3c88c35d4716dd8d02bf81ee4c240e35795aa8f
Faruk Spahic <farukspahictz@gmail.com>
2025-04-08 17:51:16 +0200

    Added custom migration files that inserts coin packages

---
+++ backend/src/drizzle/migrations/0001_seed-specific-coin-packages.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO "coinPackages" ("amount", "price") VALUES (10000, 5);
+INSERT INTO "coinPackages" ("amount", "price") VALUES (25000, 9);
+INSERT INTO "coinPackages" ("amount", "price") VALUES (50000, 15);
+INSERT INTO "coinPackages" ("amount", "price") VALUES (100000, 25);
+INSERT INTO "coinPackages" ("amount", "price") VALUES (500000, 40);
+INSERT INTO "coinPackages" ("amount", "price") VALUES (1000000, 65);
